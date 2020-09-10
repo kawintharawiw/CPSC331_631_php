3244011442e3d2e3f4a67dc0e3006a1819216714
Kawintharawiw <45837621+kawintharawiw@users.noreply.github.com>
2020-09-10 12:33:10 +0700

    Add files via upload

---
+++ northwind_update/4.2/orderdetails_update.php
@@ -0,0 +1,70 @@
+<?php 
+session_start();
+# เผยแพร่ใน http://www.thaiall.com/perlphpasp/source.pl?9137
+# ===
+# ส่วนกำหนดค่าเริ่มต้นของระบบ
+$host     = "localhost";
+$db       = "northwind";  
+$tb       = "orderdetails"; 
+$user     = "root"; // รหัสผู้ใช้ ให้สอบถามจากผู้ดูแลระบบ
+$password = "";    // รหัสผ่าน ให้สอบถามจากผู้ดูแลระบบ
+$create_table_sql = "create table orderdetails (orderid varchar(20),  productid varchar(20), quantity varchar(20))";
+if (isset($_REQUEST{'action'})) $act = $_REQUEST{'action'}; else $act = "";
+# ===
+# ส่วนแสดงผลหลัก ทั้งปกติ และหลังกดปุ่ม del หรือ edit
+if (strlen($act) == 0 || $act == "del" || $act == "edit") {
+  $conn = mysql_connect("$host","$user","$password");
+  $r = mysql_db_query($db,"select * from orderdetails") or die ("phpmyadmin - " . $create_table_sql . "<br/>" . mysql_error());
+  echo "<table>";
+  while ($o = mysql_fetch_object($r)) {
+    if (isset($_REQUEST{'orderid'}) && $_REQUEST{'orderid'}  == $o->orderid) $chg = " style='background-color:#f9f9f9"; else $chg = " readonly style='background-color:#ffffdd";
+    echo "<tr><form action='' method=post>
+      <td><input name=orderid size=5 value='". $o->orderid . "' style='background-color:#dddddd' readonly></td>
+      <td><input name=productid size=40 value='". $o->productid . "' $chg'></td>
+      <td><input name=quantity size=20 value='". $o->quantity . "' $chg;text-align:right'></td>
+      <td>";
+    if (isset($_REQUEST{'orderid'}) && $_REQUEST{'orderid'} == $o->orderid) {
+      if ($act == "del") echo "<input type=submit name=action value='del : confirm' style='height:40;background-color:yellow'>";
+      if ($act == "edit") echo "<input type=submit name=action value='edit : confirm' style='height:40;background-color:#aaffaa'>";
+    } else {
+      echo "<input type=submit name=action value='del' style='height:26'> <input type=submit name=action value='edit' style='height:26'>";
+    }
+    echo "</td></form></tr>";
+  }	
+  echo "<tr><form action='' method=post><td><input name=orderid size=5></td><td><input name=productid size=40></td><td><input name=quantity size=20></td><td><input type=submit name=action value='add' style='height:26'></td></tr>
+  </form></table>";
+  if (isset($_SESSION["msg"])) echo "<br>".$_SESSION["msg"];
+  $_SESSION["msg"] = ""; 
+  exit;
+} 
+# ===
+# ส่วนเพิ่มข้อมูล
+if ($act == "add") {
+  $q  = "insert into orderdetails values('". $_REQUEST{'orderid'} . "','". $_REQUEST{'productid'} . "','". $_REQUEST{'quantity'} . "')";
+  $conn = mysql_connect("$host","$user","$password");
+  $r = mysql_db_query($db,$q);   
+  if ($r) $_SESSION["msg"] = "insert : completely";
+  mysql_close($connect);  
+  header("Location: ". $_SERVER["SCRIPT_NAME"]);
+}
+# ===
+# ส่วนลบข้อมูล
+if ($act == "del : confirm") {
+  $q  = "delete from orderdetails where orderid ='". $_REQUEST{'orderid'} . "'";
+  $conn = mysql_connect("$host","$user","$password");
+  $r = mysql_db_query($db,$q);   
+  if ($r) $_SESSION["msg"] = "delete : completely";
+  mysql_close($connect);  
+  header("Location: ". $_SERVER["SCRIPT_NAME"]);
+}
+# ===
+# ส่วนแก้ไขข้อมูล
+if ($act == "edit : confirm") {
+  $q  = "update $tb set productid ='". $_REQUEST{'productid'} . "', quantity ='". $_REQUEST{'quantity'} . "' where orderid =" . $_REQUEST{'orderid'};
+  $conn = mysql_connect("$host","$user","$password");
+  $r = mysql_db_query($db,$q);   
+  if ($r) $_SESSION["msg"] = "edit : completely";
+  mysql_close($connect);  
+  header("Location: ". $_SERVER["SCRIPT_NAME"]);
+}
+?>
---
+++ northwind_update/4.2/suppliers_update.php
@@ -0,0 +1,70 @@
+<?php 
+session_start();
+# เผยแพร่ใน http://www.thaiall.com/perlphpasp/source.pl?9137
+# ===
+# ส่วนกำหนดค่าเริ่มต้นของระบบ
+$host     = "localhost";
+$db       = "northwind";  
+$tb       = "suppliers"; 
+$user     = "root"; // รหัสผู้ใช้ ให้สอบถามจากผู้ดูแลระบบ
+$password = "";    // รหัสผ่าน ให้สอบถามจากผู้ดูแลระบบ
+$create_table_sql = "create table suppliers (supplierid varchar(20),  companyname varchar(20), contactname varchar(20))";
+if (isset($_REQUEST{'action'})) $act = $_REQUEST{'action'}; else $act = "";
+# ===
+# ส่วนแสดงผลหลัก ทั้งปกติ และหลังกดปุ่ม del หรือ edit
+if (strlen($act) == 0 || $act == "del" || $act == "edit") {
+  $conn = mysql_connect("$host","$user","$password");
+  $r = mysql_db_query($db,"select * from suppliers") or die ("phpmyadmin - " . $create_table_sql . "<br/>" . mysql_error());
+  echo "<table>";
+  while ($o = mysql_fetch_object($r)) {
+    if (isset($_REQUEST{'supplierid '}) && $_REQUEST{'supplierid '}  == $o->supplierid ) $chg = " style='background-color:#f9f9f9"; else $chg = " readonly style='background-color:#ffffdd";
+    echo "<tr><form action='' method=post>
+      <td><input name=supplierid size=5 value='". $o->supplierid  . "' style='background-color:#dddddd' readonly></td>
+      <td><input name=companyname size=40 value='". $o->companyname . "' $chg'></td>
+      <td><input name=contactname size=20 value='". $o->contactname . "' $chg;text-align:right'></td>
+      <td>";
+    if (isset($_REQUEST{'supplierid'}) && $_REQUEST{'supplierid'} == $o->supplierid) {
+      if ($act == "del") echo "<input type=submit name=action value='del : confirm' style='height:40;background-color:yellow'>";
+      if ($act == "edit") echo "<input type=submit name=action value='edit : confirm' style='height:40;background-color:#aaffaa'>";
+    } else {
+      echo "<input type=submit name=action value='del' style='height:26'> <input type=submit name=action value='edit' style='height:26'>";
+    }
+    echo "</td></form></tr>";
+  }	
+  echo "<tr><form action='' method=post><td><input name=supplierid size=5></td><td><input name=companyname size=40></td><td><input name=contactname size=20></td><td><input type=submit name=action value='add' style='height:26'></td></tr>
+  </form></table>";
+  if (isset($_SESSION["msg"])) echo "<br>".$_SESSION["msg"];
+  $_SESSION["msg"] = ""; 
+  exit;
+} 
+# ===
+# ส่วนเพิ่มข้อมูล
+if ($act == "add") {
+  $q  = "insert into suppliers values('". $_REQUEST{'supplierid'} . "','". $_REQUEST{'companyname'} . "','". $_REQUEST{'contactname'} . "')";
+  $conn = mysql_connect("$host","$user","$password");
+  $r = mysql_db_query($db,$q);   
+  if ($r) $_SESSION["msg"] = "insert : completely";
+  mysql_close($connect);  
+  header("Location: ". $_SERVER["SCRIPT_NAME"]);
+}
+# ===
+# ส่วนลบข้อมูล
+if ($act == "del : confirm") {
+  $q  = "delete from suppliers where supplierid ='". $_REQUEST{'supplierid'} . "'";
+  $conn = mysql_connect("$host","$user","$password");
+  $r = mysql_db_query($db,$q);   
+  if ($r) $_SESSION["msg"] = "delete : completely";
+  mysql_close($connect);  
+  header("Location: ". $_SERVER["SCRIPT_NAME"]);
+}
+# ===
+# ส่วนแก้ไขข้อมูล
+if ($act == "edit : confirm") {
+  $q  = "update $tb set companyname ='". $_REQUEST{'companyname'} . "', contactname ='". $_REQUEST{'contactname'} . "' where supplierid =" . $_REQUEST{'supplierid'};
+  $conn = mysql_connect("$host","$user","$password");
+  $r = mysql_db_query($db,$q);   
+  if ($r) $_SESSION["msg"] = "edit : completely";
+  mysql_close($connect);  
+  header("Location: ". $_SERVER["SCRIPT_NAME"]);
+}
+?>
